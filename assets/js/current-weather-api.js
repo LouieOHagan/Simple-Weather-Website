5c6c709f9df73dce7afac4e4bc8607595662869f
Louie O Hagan <louieohagan101@gmail.com>
2020-03-04 20:12:05 +0000

    Connected API using xhr/ajax. Successfully console logged current weather data in london after clicking search button for testing

--- assets/js/current-weather-api.js
+++ assets/js/current-weather-api.js
@@ -1,7 +1,23 @@
 console.log("Test");
 
 document.getElementById("searchButton").addEventListener("click", searchFunction);
+const link = "https://api.openweathermap.org/data/2.5/weather?q=london";
+const apiKey = "&appid=308bcfc339b942ce47cc8a976f8c4728";
 
 function searchFunction(){
     console.log("Button works!")
+    let url = link + apiKey;
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("GET", url, true);
+    
+    xhr.onload = function() {
+        if(this.readyState === 4 && this.status === 200){
+            console.log("I'm ready!");
+            console.log(xhr.responseText)
+        }
+    };
+
+    xhr.send();
 }
