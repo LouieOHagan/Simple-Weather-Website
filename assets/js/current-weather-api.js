09cab2c2bdc5b1599ac980b5c06a660b1f01d676
Louie O Hagan <louieohagan101@gmail.com>
2020-03-04 20:59:13 +0000

    Got data printing to page and added comments to Javascript for ease of use

--- assets/js/current-weather-api.js
+++ assets/js/current-weather-api.js
@@ -3,23 +3,24 @@ const link = "https://api.openweathermap.org/data/2.5/weather?q=";
 const apiKey = "&appid=308bcfc339b942ce47cc8a976f8c4728";
 
 function searchFunction(){
-    let userInput = document.getElementById("searchInput").value + "&units=metric";
-    console.log(userInput);
-    let url = link + userInput + apiKey;
+    // Gets search box value and converts units to metric to be added to url
+    let userInput = document.getElementById("searchInput").value + "&units=metric";     
+    let url = link + userInput + apiKey;    // adds endpoint, API Key and UserInput together to form link to be called
 
     let xhr = new XMLHttpRequest();
-
-    xhr.open("GET", url, true);
+    xhr.open("GET", url, true);             // Initializes request (request = xhr variable)
     
     xhr.onload = function() {
         if(this.readyState === 4 && this.status === 200){
             resultsFunction(xhr.responseText);
         }
     };
-
-    xhr.send();
+    
+    xhr.send();                             // Sends request to server
 }
 
 function resultsFunction(weatherData) {
-    console.log(weatherData);
+    weatherData = JSON.parse(weatherData);  // Parses incoming data (weatherData) into JS Object Format (JSON)
+
+    document.getElementById("searchResults").innerHTML = `Weather: ${weatherData.weather[0].main}`;
 }
