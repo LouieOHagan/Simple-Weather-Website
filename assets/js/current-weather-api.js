9f6303b99afa8a77b44f60563f79c217c090464a
Louie O Hagan <louieohagan101@gmail.com>
2020-03-05 12:48:32 +0000

    Cleaned up code: adjusted if else statements, if minutes and hours are under 10 to 2 if statements and declared sunSetTime variable below

--- assets/js/current-weather-api.js
+++ assets/js/current-weather-api.js
@@ -23,32 +23,36 @@ function resultsFunction(weatherData) {
     weatherData = JSON.parse(weatherData);  // Parses incoming data (weatherData) into JS Object Format (JSON)
 
     // Variable adding sunrise time and timezone unix values & converting them to timestamps
+    // Converts value from seconds to milliseconds (* 1000)
     let sunrise = new Date((weatherData.sys.sunrise + weatherData.timezone) * 1000);
     let sunRiseHours = sunrise.getHours();          // Variable getting hours value from timestamp
     let sunRiseMinutes = sunrise.getMinutes();      // Variable getting minutes value from timestamp
-    let sunRiseTime;                                // declaring sun rise time variable to be called in if else statement
 
-    if (sunRiseHours < 10 && sunRiseMinutes < 10) {
-        sunRiseTime = `0${sunRiseHours}:0${sunRiseMinutes}`;
-    } else if (sunRiseHours < 10){
-        sunRiseTime = `0${sunRiseHours}:${sunRiseMinutes}`;
-    } else if (sunRiseMinutes < 10) {
-        sunRiseTime = `${sunRiseHours}:0${sunRiseMinutes}`;
-    } else {
-        sunRiseTime = `${sunRiseHours}:${sunRiseMinutes}`;
+    if(sunRiseHours < 10){
+        sunRiseHours = `0${sunRiseHours}`;
     }
+
+    if(sunRiseMinutes < 10){
+        sunRiseMinutes = `0${sunRiseMinutes}`;
+    }
+
+    let sunRiseTime = `${sunRiseHours}:${sunRiseMinutes}`;
 
     // Variable adding sunset time and timezone unix values & converting them to timestamps
+    // Converts value from seconds to milliseconds (* 1000)
     let sunset = new Date((weatherData.sys.sunset + weatherData.timezone) * 1000);
     let sunSetHours = sunset.getHours();            // Variable getting hours value from timestamp
-    let sunSetMinutes = sunset.getMinutes();        // Variable getting minutes value from timestamp
-    let sunSetTime;                                 // declaring sun rise time variable to be called in if else statement
+    let sunSetMinutes = sunset.getMinutes();        // Variable getting minutes value from timestamp  
 
     if(sunSetMinutes < 10){
-        sunSetTime = `${sunSetHours}:0${sunSetMinutes}`;
-    } else {
-        sunSetTime = `${sunSetHours}:${sunSetMinutes}`;
+        sunSetMinutes = `0${sunSetMinutes}`;
+    } 
+
+    if(sunSetHours < 10){
+        sunSetHours = `0${sunSetHours}`;
     }
+
+    let sunSetTime = `${sunSetHours}:${sunSetMinutes}`;  
     
     let output = "";
 
